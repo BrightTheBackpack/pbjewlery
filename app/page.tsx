a8ac2a2e011f40a9be7087df9e90009f85b4a478
BrightTheBackpack <78445409+BrightTheBackpack@users.noreply.github.com>
2023-11-09 09:08:42 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
-
+import MainNavBar from 'app/navbar.tsx'
 export default function Home() {
   return <p> Paper Boat Jewlery </p>
+          <div></div>
+            <MainNavBar></MainNavBar>
     
 }
